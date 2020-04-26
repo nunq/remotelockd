e791daf9c6b601e87e364d80d225e396c5623174
hyphenc <46054695+hyphenc@users.noreply.github.com>
2020-04-26 15:08:00 +0200

    simplify script

--- suspendpc.sh
+++ suspendpc.sh
@@ -7,8 +7,4 @@ PORT=""
 printf "$TOKEN" | nc "$IP" "$PORT"
 
 sleep 2
-if ping -c 3 -w 3 "$IP"; then
-  echo "couldn't suspend. host is still up."
-else
-  echo "suspended. host is down."
-fi
+ping -c 3 -w 3 "$IP"
